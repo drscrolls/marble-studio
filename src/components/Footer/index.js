b522a2ff124e01908b5e535f34ccfb19f4fd9429
andrew <andrew@hubtel.com>
2022-06-18 12:50:20 +0000

    started footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,43 @@ import "./style.css";
 const Footer = () => {
   return (
     <div className="footerContainer">
-      
+      <Container>
+        <Row xs={1} md={1} lg={2}>
+          <Col>
+            <div className="n_title">SIGN UP TO OUR NEWSLETTER:</div>
+          </Col>
+          <Col>
+            <div></div>
+          </Col>
+        </Row>
+        <Row xs={1} md={1} lg={1}>
+          <Col>
+            <div className="input_container">
+              <input className="input" placeholder="your@email.com" />
+              <div className="button_container" >
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div></div>
+          </Col>
+        </Row>
+        <Row xs={1} md={1} lg={1}>
+          <Col>
+            <div style={{float: "left", marginTop: 10}}>
+
+              
+
+            </div>
+          </Col>
+          <Col>
+            <div></div>
+          </Col>
+        </Row>
+      </Container>
+        
+          
+
     </div>
   )
 }
