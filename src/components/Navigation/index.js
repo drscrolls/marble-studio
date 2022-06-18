0626c264b94e3f2283557181964d39ff2ab81891
andrew <andrew@hubtel.com>
2022-06-18 15:34:33 +0000

    set header position to fixed

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
 import "./style.css";
 
 const logoWhite = require("../../assets/logo-white.png");
 
 const Header = () => {
   return (
-    <div>
+    <div className="navContainer">
         <div className="nav">
             <div className="navLeft">
               <div className="logoContainer">
